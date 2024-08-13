ff2d4119c9dc79d3c62644868ebc6557f7b0a1f6
bravin-onwonga <onbravin@gmail.com>
2024-08-14 01:01:51 +0300

    Find, filter and sort

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
-"""Lists all documents in a collection"""
+"""Students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    student_lst = []
+
+    for student in mongo_collection.find():
+        total = 0
+        new_dict = {}
+        new_dict["name"] = student.get('name')
+        new_dict['id'] = student.get('_id')
+        num = len(student.get('topics'))
+        for topic in student.get('topics'):
+            total += topic.get('score')
+        avg = total / num
+        new_dict['averageScore'] = avg
+        student_lst.append(new_dict)
+
+    return student_lst.sort(key=myFunc, reverse=True)
+
+def myFunc(e):
+  return e['averageScore']
