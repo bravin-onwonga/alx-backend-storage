3e37410ba32ab6a04100f9fbf52f181a08822b9e
bravin-onwonga <onbravin@gmail.com>
2024-08-14 16:11:54 +0300

    First task of redis basics

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Writing to redis"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache(object):
+    def __init__(self) -> None:
+        """Instantiates a cache object"""
+        _redis = redis.Redis()
+        _redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores data into a redis db
+        Params:
+            data: can be str, bytes, int, float
+        Returns: unique key generated
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
