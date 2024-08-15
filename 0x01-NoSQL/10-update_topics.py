078d03f5c8fc6b7422b6e5ed98b0ab9f3c7c3944
bravin-onwonga <onbravin@gmail.com>
2024-08-15 18:12:38 +0300

    Using update many to update all documents

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """Updates the value of a document"""
     query = {"name": name}
     new_topics = {"$set": {"topics": topics}}
-    mongo_collection.update_one(query, new_topics)
+    mongo_collection.update_many(query, new_topics)
