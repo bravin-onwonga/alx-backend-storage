83163e8a76216a0147328ee4a6fbbdda7b52a802
bravin-onwonga <onbravin@gmail.com>
2024-08-15 18:39:13 +0300

    fix:Sorting in reverse and using key _id

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,20 +5,28 @@
 def top_students(mongo_collection):
     """Returns all students sorted by average score"""
     student_lst = []
+    average_list = []
+    sorted_student_lst = []
 
     for student in mongo_collection.find():
         total = 0
         new_dict = {}
         new_dict["name"] = student.get('name')
-        new_dict['id'] = student.get('_id')
+        new_dict['_id'] = student.get('_id')
         num = len(student.get('topics'))
         for topic in student.get('topics'):
             total += topic.get('score')
         avg = total / num
+        average_list.append(avg)
         new_dict['averageScore'] = avg
         student_lst.append(new_dict)
 
-    return student_lst.sort(key=myFunc, reverse=True)
+    sorted_avgs = sorted(average_list, reverse=True)
 
-def myFunc(e):
-  return e['averageScore']
+    for i in sorted_avgs:
+        for stud in student_lst:
+            if stud.get('averageScore') == i:
+                sorted_student_lst.append(stud)
+                break
+
+    return sorted_student_lst
