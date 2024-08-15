9ccc01af6256dc7b21d4b17535736e9e415bc36b
bravin-onwonga <onbravin@gmail.com>
2024-08-15 19:00:50 +0300

    Setting up

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
-"""Lists all documents in a collection"""
+"""Print logs from a nginx server"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.log.nginx
+
+    logs_data = nginx_collection.find()
+    print(logs_data)
